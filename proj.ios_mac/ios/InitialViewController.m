a816feb92f214c97a784bf70a3f17f37b7c60fb8
Raúl Uranga <ruranga@gmail.com>
2016-05-26 11:54:54 -0500

    remove editable text field removed, in favor of two buttons.

--- proj.ios_mac/ios/InitialViewController.m
+++ proj.ios_mac/ios/InitialViewController.m
@@ -19,74 +19,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.initialTextField.delegate=self;
-    // Do any additional setup after loading the view from its nib.
-    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
-- (IBAction)goButtonAction:(id)sender {
+- (IBAction)modalButtonAction:(id)sender {
     
+    GameViewController *gameView = [[GameViewController alloc]init];
+    gameView.presentationStyle=@"Modal";
+    [self presentViewController:gameView animated:YES completion:nil];
+            
+}
 
-    if(self.initialTextField.isFirstResponder){
+
+- (IBAction)swButtonAction:(id)sender {
+    
+    MenuViewController *rightViewController = [[MenuViewController alloc] init];
+    GameViewController *frontViewController = [[GameViewController alloc] init];
+    frontViewController.presentationStyle=@"Navigation";
+    SWRevealViewController *mainRevealController = [[SWRevealViewController alloc] init];
+    [mainRevealController setRightViewController:rightViewController];
+    [mainRevealController setFrontViewController:frontViewController];
         
-        [self.initialTextField resignFirstResponder];
-    
-    }
-    
-        
-        if([[self.initialTextField.text stringByReplacingOccurrencesOfString:@" " withString:@""] caseInsensitiveCompare:@"Modal"] == NSOrderedSame){
-            
-            GameViewController *gameView = [[GameViewController alloc]init];
-            gameView.presentationStyle=@"Modal";
-            [self presentViewController:gameView animated:YES completion:nil];
-            
-        }else if([[self.initialTextField.text stringByReplacingOccurrencesOfString:@" " withString:@""] caseInsensitiveCompare:@"Navigation"] == NSOrderedSame){
-            
-            MenuViewController *rightViewController = [[MenuViewController alloc] init];
-            GameViewController *frontViewController = [[GameViewController alloc] init];
-            frontViewController.presentationStyle=@"Navigation";
-            SWRevealViewController *mainRevealController = [[SWRevealViewController alloc] init];
-            [mainRevealController setRightViewController:rightViewController];
-            [mainRevealController setFrontViewController:frontViewController];
-            
-            [[[UIApplication sharedApplication]delegate] window].rootViewController=mainRevealController;
-            [[[[UIApplication sharedApplication]delegate] window] makeKeyAndVisible];
-        }else{
-        
-        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Error" message:@"You have entered an invalid choice.Please enter again correctly." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
-        [alert show];
-    }
+    [[[UIApplication sharedApplication]delegate] window].rootViewController=mainRevealController;
+    [[[[UIApplication sharedApplication]delegate] window] makeKeyAndVisible];
 }
 
--(void)textFieldDidBeginEditing:(UITextField *)textField{
-    
-    [textField becomeFirstResponder];
-}
-
--(void)textFieldDidEndEditing:(UITextField *)textField{
-    
-    [textField resignFirstResponder];
-}
-
--(BOOL)textFieldShouldReturn:(UITextField *)textField{
-    
-    [textField resignFirstResponder];
-    
-    return true;
-}
 @end
